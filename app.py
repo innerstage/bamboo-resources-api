4c1dd7953c5b56223a5e2602ab9c022be76a46ee
domsmo <dmoenne09@gmail.com>
2021-12-10 13:10:39 -0300

    Deleted useless lines

--- app.py
+++ app.py
@@ -1,12 +1,3 @@
-# from flask import Flask
-
-# app = Flask(__name__)
-
-# @app.route("/greeting")
-# def hello_world():
-#     return "<h1>Hello, World!</h1><p>Wena Domi</p>"
-
-
 from flask import Flask, request, send_from_directory
 
 # set the project root directory as the static folder, you can set others.
